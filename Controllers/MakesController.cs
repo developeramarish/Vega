7666230f0659b70bdef0647379818a29f5afed66
mpsingh17 <mr.jassal1722@gmail.com>
2020-03-07 11:54:21 +1300

    get makes API action.

---
+++ Controllers/MakesController.cs
@@ -0,0 +1,25 @@
+using System.Collections.Generic;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using Vega.Data.Models;
+using Vega.Persistence;
+
+namespace Vega.Controllers
+{
+    public class MakesController : Controller
+    {
+        private readonly VegaDbContext context;
+
+        public MakesController(VegaDbContext context)
+        {
+            this.context = context;
+        }
+        
+        [HttpGet("/api/makes")]
+        public async Task<IEnumerable<Make>> GetMakes()
+        {
+            return await context.Makes.Include(make => make.Models).ToListAsync();
+        }
+    }
+}
