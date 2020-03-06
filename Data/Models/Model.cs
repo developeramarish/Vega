65593b9647070e56c57db2c37da3aaf2da0a27e0
mpsingh17 <mr.jassal1722@gmail.com>
2020-03-06 18:59:45 +1300

    make and model domain models.

---
+++ Data/Models/Model.cs
@@ -0,0 +1,12 @@
+namespace Vega.Data.Models
+{
+    public class Model
+    {
+        public int Id { get; set; }
+
+        public string Name { get; set; }
+
+        public Make Make { get; set; }
+        public int MakeId { get; set; }
+    }
+}
