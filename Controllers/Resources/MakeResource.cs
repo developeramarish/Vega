14e63c63af696ee751d0ce790adab388f74641e9
mpsingh17 <mr.jassal1722@gmail.com>
2020-03-07 12:15:45 +1300

    API resources for Make and Model.

---
+++ Controllers/Resources/MakeResource.cs
@@ -0,0 +1,20 @@
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using Vega.Models;
+
+namespace Vega.Controllers.Resources
+{
+    public class MakeResource
+    {
+        public int Id { get; set; }
+
+        public string Name { get; set; }
+
+        public ICollection<ModelResource> Models { get; set; }
+
+        public MakeResource()
+        {
+            Models = new Collection<ModelResource>();
+        }
+    }
+}
---
+++ Controllers/Resources/ModelResource.cs
@@ -0,0 +1,12 @@
+using Vega.Models;
+
+namespace Vega.Controllers.Resources
+{
+    public class ModelResource
+    {
+        public int Id { get; set; }
+
+        public string Name { get; set; }
+        
+    }
+}
