70461f976bb54b8b7ebe931b1489dde46b04de9d
mpsingh17 <mr.jassal1722@gmail.com>
2020-03-06 19:40:19 +1300

    configure db context and initial model.

--- Startup.cs
+++ Startup.cs
@@ -3,8 +3,10 @@ using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.HttpsPolicy;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.SpaServices.AngularCli;
+using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
+using Vega.Persistence;
 
 namespace Vega
 {
@@ -22,6 +24,8 @@ namespace Vega
         {
             services.AddMvc().SetCompatibilityVersion(CompatibilityVersion.Version_2_1);
 
+            services.AddDbContext<VegaDbContext>(options => options.UseSqlServer(Configuration.GetConnectionString("Default")));
+ 
             // In production, the Angular files will be served from this directory
             services.AddSpaStaticFiles(configuration =>
             {
