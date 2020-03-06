a591081265c73b2c2e3bd0c5be87cc7a6af22314
mpsingh17 <mr.jassal1722@gmail.com>
2020-03-07 12:40:42 +1300

    AutoMapper mapping profile.

---
+++ Mapping/MappingProfile.cs
@@ -0,0 +1,16 @@
+using AutoMapper;
+using Vega.Controllers.Resources;
+using Vega.Models;
+
+namespace Vega.Mapping
+{
+    public class MappingProfile : Profile
+    {
+        public MappingProfile()
+        {
+            CreateMap<Make, MakeResource>();
+            CreateMap<Model, ModelResource>();
+        }
+        
+    }
+}
