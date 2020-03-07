debe1f6cf3c373d415d4a3fc6532fe330dc65faa
mpsingh17 <mr.jassal1722@gmail.com>
2020-03-07 15:29:19 +1300

    implement get features.

---
+++ Controllers/FeaturesController.cs
@@ -0,0 +1,30 @@
+using System.Collections.Generic;
+using System.Linq;
+using AutoMapper;
+using Microsoft.AspNetCore.Mvc;
+using Vega.Controllers.Resources;
+using Vega.Models;
+using Vega.Persistence;
+
+namespace Vega.Controllers
+{
+    public class FeaturesController : Controller
+    {
+        private readonly VegaDbContext context;
+        private readonly IMapper mapper;
+
+        public FeaturesController(VegaDbContext context, IMapper mapper)
+        {
+            this.mapper = mapper;
+            this.context = context;
+        }
+
+        [HttpGet("/api/features")]
+        public IEnumerable<FeatureResource> GetFeatures()
+        {
+            var features = context.Features.ToList();
+
+            return mapper.Map<List<Feature>, List<FeatureResource>>(features);
+        }
+    }
+}
--- Mapping/MappingProfile.cs
+++ Mapping/MappingProfile.cs
@@ -10,6 +10,8 @@ namespace Vega.Mapping
         {
             CreateMap<Make, MakeResource>();
             CreateMap<Model, ModelResource>();
+            CreateMap<Feature, FeatureResource>();
+
         }
         
     }
