8e697cf451e3d8b0dca24c7633b94a0071d5f590
mpsingh17 <mr.jassal1722@gmail.com>
2020-03-08 21:57:53 +1300

    vehicle model and vehicle features many to many.

--- Persistence/VegaDbContext.cs
+++ Persistence/VegaDbContext.cs
@@ -5,13 +5,18 @@ namespace Vega.Persistence
 {
     public class VegaDbContext : DbContext
     {
+        public DbSet<Make> Makes { get; set; }
+        public DbSet<Model> Models { get; set; }
+        public DbSet<Feature> Features { get; set; }
+
         public VegaDbContext(DbContextOptions<VegaDbContext> options) : base(options)
         {    
         }
 
-        public DbSet<Make> Makes { get; set; }
-        public DbSet<Model> Models { get; set; }
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<VehicleFeature>().HasKey(vf => new { vf.VehicleId, vf.FeatureId });
+        }
 
-        public DbSet<Feature> Features { get; set; }
     }
 }
